41f1a46ad93e9f37d62a96a2b9573cd43aabb73a
Dawid Spisak <dawidspisak@gmail.com>
2021-08-31 21:20:18 +0200

    add icons

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const SidebarOption = () => {
+  return (
+    <SidebarOptionContainer>
+      <h1>test</h1>
+    </SidebarOptionContainer>
+  );
+};
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div``;
