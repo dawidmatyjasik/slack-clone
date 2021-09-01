102e27927f4f7f8484ceb8c4864ee0123f4a66a4
Dawid Spisak <dawidspisak@gmail.com>
2021-09-01 20:02:30 +0200

    change routing

--- src/App.js
+++ src/App.js
@@ -5,22 +5,42 @@ import Header from "./components/Header";
 import styled from "styled-components";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="http://assets.stickpng.com/images/5cb480cd5f1b6d3fbadece79.png"
+            alt=""
+          />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
   return (
     <>
       <Router>
-        <>
-          <Header />
-          <AppBody>
-            <Sidebar />
-            <Switch>
-              <Route path="/" exact>
-                <Chat />
-              </Route>
-            </Switch>
-          </AppBody>
-        </>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <AppBody>
+              <Sidebar />
+              <Switch>
+                <Route path="/" exact>
+                  <Chat />
+                </Route>
+              </Switch>
+            </AppBody>
+          </>
+        )}
       </Router>
     </>
   );
@@ -31,4 +51,25 @@ export default App;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `;
